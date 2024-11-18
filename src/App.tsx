0517e95296d916dc9645bed864b61d2eda0bff8c
Guillermo Sotelo <guille.sotelo.cloud@gmail.com>
2024-11-18 15:58:30 +0100

    uis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Chat } from "./pages/Chat";
 import { AppProvider } from './AppContext';
 import './sass/app.scss'
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <AppProvider>
+      <ToastContainer/>
       <Chat />
     </AppProvider>
   );
