74cf379531b640694004ff855451f35aa1084cbb
Guillermo <guille.sotelo.cloud@gmail.com>
2025-04-02 09:44:33 +0200

    analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './AppContext';
-import GoogleAnalytics from './components/GoogleAnalytics';
 import ReactGA from 'react-ga4';
 const TRACKING_ID = "G-V6HK4YH119";
-ReactGA.initialize(TRACKING_ID);
+ReactGA.initialize(TRACKING_ID, {
+  gaOptions: {
+    anonymizeIp: true,
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,7 +18,6 @@ root.render(
   // Strict mode renders everythin twice in dev mode. Leave this commented.
   // <React.StrictMode>
   <AppProvider>
-    <GoogleAnalytics />
     <App />
   </AppProvider>
   // </React.StrictMode>
