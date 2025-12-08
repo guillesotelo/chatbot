7ed3d548fec5083de3ac7f806c5fbce45535e4d6
Guillermo <guillermo.sotelo@volvocars.com>
2025-12-08 14:07:29 +0100

    dlt-checkr

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { htmlBuildingTemplate } = require("./src/templates/buildingTemplate")
 const dotenv = require('dotenv')
 dotenv.config()
 const { createProxyMiddleware } = require("http-proxy-middleware"); // Import the proxy middleware
+const PORT = process.env.PORT || 3000
 
 app.use((_, res, next) => {
     res.setHeader('Cache-Control', 'no-store')
@@ -34,7 +35,26 @@ app.use((err, _, res, __) => {
     res.status(500).send("Something broke!")
 })
 
-const PORT = process.env.PORT || 3000
+const ANALYZER_BUILD_PATH = process.env.ANALYZER_BUILD_PATH || "/dlt-gdpr-analyzer/ui/build"
+const analyzerIndexPath = path.resolve(ANALYZER_BUILD_PATH, "index.html")
+const analyzerExists = fs.existsSync(analyzerIndexPath)
+
+if (analyzerExists) {
+  app.use('/dlt-gdpr-analyzer', express.static(ANALYZER_BUILD_PATH))
+
+  app.get('/dlt-gdpr-analyzer*', (req, res) => {
+    res.sendFile(analyzerIndexPath, (err) => {
+      if (err) {
+        console.error('Error sending analyzer index.html', err)
+        res.status(500).send('Analyzer build not available')
+      }
+    })
+  })
+
+  console.log('[INFO] Analyzer build served at /dlt-gdpr-analyzer from', ANALYZER_BUILD_PATH)
+} else {
+  console.log('[INFO] No analyzer build found at', ANALYZER_BUILD_PATH)
+}
 
 const buildPath = path.resolve(__dirname, 'build') || ''
 const indexHtmlPath = buildPath ? path.resolve(buildPath, 'index.html') : ''
